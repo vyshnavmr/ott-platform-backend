7941c17e22c0727bbb06feed6305b45ea5fa0e57
Vyshnav <vyshnav2004sk@gmail.com>
2026-07-03 16:58:59 +0530

    CORS edit

--- ott/settings.py
+++ ott/settings.py
@@ -70,7 +70,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://ott-platform-frontend-vercel-2.vercel.app/",
+    "https://ott-platform-frontend-vercel-2.vercel.app",
 ]
 
 ROOT_URLCONF = 'ott.urls'
